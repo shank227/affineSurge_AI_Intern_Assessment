b6c198f2b394787209719a898b4beace1fa25a52
shank227 <82793911+shank227@users.noreply.github.com>
2026-07-17 20:34:55 +0530

    basic presentation of comparison manuals

--- test_versioning.py
+++ test_versioning.py
@@ -2,4 +2,14 @@ from app.versioning import compare_versions
 
 result = compare_versions("v1", "v2")
 
-print(result)
+print("Changed:")
+for item in result["changed"]:
+    print(item)
+
+print("\nNew:")
+for item in result["new"]:
+    print(item)
+
+print("\nRemoved:")
+for item in result["removed"]:
+    print(item)
